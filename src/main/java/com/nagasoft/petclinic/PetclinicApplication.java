3a060cfaa8d1393962a26e5d8ca1e4132601cb84
Nagarjun16 <nagarjun16@gmail.com>
2020-12-27 14:35:57 +0530

    Update PetclinicApplication.java

--- src/main/java/com/nagasoft/petclinic/PetclinicApplication.java
+++ src/main/java/com/nagasoft/petclinic/PetclinicApplication.java
@@ -2,12 +2,17 @@ package com.nagasoft.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class PetclinicApplication {
 
+	private static ConfigurableApplicationContext ctx;
+
 	public static void main(String[] args) {
-		SpringApplication.run(PetclinicApplication.class, args);
+		ctx=SpringApplication.run(PetclinicApplication.class, args);
+		Object bean = ctx.getBean("BookController");
+		System.out.println(bean);
 	}
 
 }
